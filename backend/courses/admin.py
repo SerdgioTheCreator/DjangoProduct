bd8d938103da708045d0ec435420d821f433786e
Сергей Коновалов <SokiZNosoK68rus@yandex.ru>
2025-01-05 22:45:52 +0300

    Course and CourseAdmin models added

--- backend/courses/admin.py
+++ backend/courses/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from courses.models import Course
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'author', 'title',
+        'description', 'price', 'is_active'
+    )
+    list_filter = ('author', 'price', 'is_active',)
